c7557f8bc975058bb2dd06431741f87a72a0cd39
FaizAlam4 <faizalam4e@gmail.com>
2024-01-09 15:44:34 +0530

    added responsiveness

--- src/Components/BoardLayOut.jsx
+++ src/Components/BoardLayOut.jsx
@@ -3,24 +3,26 @@ import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 function BoardLayOut({ data }) {
   return (
-    <Grid
-      item
-      xs={3.6}
-      sx={{
+  <div
+      style={{
         height: "150px",
-        width: "80px",
+        width: "350px",
         margin: "10px",
         background: `linear-gradient(${data.prefs.backgroundTopColor},${data.prefs.backgroundBottomColor})`,
         borderRadius: "5px",
         fontWeight: "bold",
+        padding:'10px',
         fontSize: "1.2rem",
-        color: "white"
+        color: "white",
       }}
     >
-      <Link to={`/${data.id}`} style={{textDecoration:'none', color:'inherit'}}>
-        <div style={{ width: "300px", height: "100px"}}>{data.name}</div>
+      <Link
+        to={`/${data.id}`}
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div style={{ width: "300px", height: "100px" }}>{data.name}</div>
       </Link>
-    </Grid>
+    </div>
   );
 }
 
--- src/Components/BoardSection.jsx
+++ src/Components/BoardSection.jsx
@@ -6,8 +6,8 @@ import Box from "@mui/material/Box";
 import Logo from "../assets/formLogo.svg";
 import Popper from "@mui/material/Popper";
 
-import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
+// import Grid from "@mui/material/Grid";
+// import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 
@@ -65,84 +65,84 @@ function BoardSection() {
 
   return (
     <>
-      <Container sx={{ width: "100vw", display: "relative" }}>
-        <Grid
-          container
-          spacing={3}
-          sx={{
-            width: "100%",
-            paddingTop: "70px",
-            justifyContent: "space-between",
-          }}
-        >
-          <div>
-            <div
-              style={{
-                fontSize: "1.2rem",
-                backgroundColor: "#091e420f",
-                color: "#172b4d",
-                textAlign: "center",
-                lineHeight: "4rem",
-                borderRadius: "5px",
-                width: "350px",
-                height: "150px",
-                marginTop: "10px",
-                marginLeft: "8px",
-              }}
-              aria-describedby={id}
-              type="button"
-              onClick={handleClick}
+    <div
+        style={{
+          width: "90%",
+          display: "flex",
+          flexFlow: "row wrap",
+          paddingTop: "70px",
+          justifyContent: "space-between",
+          margin: "auto",
+        }}
+      >
+        <div style={{ minWidth: "350px" }}>
+          <div
+            style={{
+              fontSize: "1.2rem",
+              backgroundColor: "#091e420f",
+              color: "#172b4d",
+              textAlign: "center",
+              lineHeight: "4rem",
+              borderRadius: "5px",
+              width: "100%",
+              height: "150px",
+              marginTop: "10px",
+              marginLeft: "8px",
+            }}
+            aria-describedby={id}
+            type="button"
+            onClick={handleClick}
+          >
+            Create new board
+            <Typography
+              sx={{ display: "block" }}
+              variant="caption"
+              component="caption"
             >
-              Create new board
-              <Typography
-                sx={{ display: "block" }}
-                variant="caption"
-                component="caption"
-              >
-                remaining {10 - data.length}
-                <HelpOutlineOutlinedIcon
-                  sx={{
-                    fontSize: "1rem",
-                    display: "block",
-                    marginTop: "13px",
-                    marginLeft: "260px",
-                  }}
-                />
-              </Typography>
-            </div>
-            <Popper id={id} open={open} anchorEl={anchorEl}>
-              <Box sx={{ border: 1, p: 1, bgcolor: "white" }}>
-                <div className="form">
-                  <img className="ig" src={Logo} alt="" />
-                  <label
-                    style={{
-                      marginTop: "70px",
-                      display: "block",
-                      padding: "15px",
-                    }}
-                  >
-                    Board Title <small style={{ color: "red" }}>*</small>
-                  </label>
-                  <input type="text" value={inp} onChange={handleInput} />
-
-                  <button
-                    onClick={() => {
-                      handleForm(inp);
-                    }}
-                    className="btn"
-                  >
-                    Create
-                  </button>
-                </div>
-              </Box>
-            </Popper>
+              remaining {10 - data.length}
+              <HelpOutlineOutlinedIcon
+                sx={{
+                  fontSize: "1rem",
+                  display: "block",
+                  marginTop: "13px",
+                  marginLeft: "260px",
+                }}
+              />
+            </Typography>
           </div>
+          <Popper id={id} open={open} anchorEl={anchorEl}>
+            <Box sx={{ border: 1, p: 1, bgcolor: "white" }}>
+              <div className="form">
+                <img className="ig" src={Logo} alt="" />
+                <label
+                  style={{
+                    marginTop: "70px",
+                    display: "block",
+                    padding: "15px",
+                  }}
+                >
+                  Board Title <small style={{ color: "red" }}>*</small>
+                </label>
+                <input type="text" value={inp} onChange={handleInput} />
 
-          {data.map((board, index) => (
-            <BoardLayOut key={index} id={board.id} data={board} />
-          ))}
-        </Grid>
-      </Container>
+                <button
+                  onClick={() => {
+                    handleForm(inp);
+                  }}
+                  className="btn"
+                >
+                  Create
+                </button>
+              </div>
+            </Box>
+          </Popper>
+        </div>
+
+        {data.map((board, index) => (
+          <BoardLayOut key={index} id={board.id} data={board} />
+        ))}
+      </div>
+
     </>
   );
 }
