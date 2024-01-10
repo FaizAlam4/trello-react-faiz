2bbbedf58db5ba98043d7c3559a718d1d42fad25
FaizAlam4 <faizalam4e@gmail.com>
2024-01-10 23:38:11 +0530

    checkitem

--- src/Components/CardView.jsx
+++ src/Components/CardView.jsx
@@ -6,9 +6,9 @@ import CloseIcon from "@mui/icons-material/Close";
 import DomainVerificationIcon from "@mui/icons-material/DomainVerification";
 import { useEffect, useState } from "react";
 import ChecklistView from "./ChecklistView";
-import Box from '@mui/material/Box';
-import { ClickAwayListener } from '@mui/base/ClickAwayListener';
-import AddIcon from '@mui/icons-material/Add';
+import Box from "@mui/material/Box";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
+import AddIcon from "@mui/icons-material/Add";
 import axios from "axios";
 
 function CardView() {
@@ -27,35 +27,38 @@ function CardView() {
     setOpen(false);
   };
 
-  const createChecklist=()=>{
-    axios.post(`https://api.trello.com/1/cards/${id2}/checklists?name=${inputval}&key=688828938a0a81fbaff1c76c5dfa1577&token=ATTA8f44402b42b106239bf6db2011236ca301fa1f2e7c2bd2e8a8766b79af386751A34FF02D`).then((data)=>{
-     setChecklistdata([...checklistData,data.data])
-     setOpen(false)
-
-    }).catch((err)=>{
-      console.log(err)
-      setOpen(false)
-      alert("Couldn't create the checklist")
-    })
-  }
+  const createChecklist = () => {
+    axios
+      .post(
+        `https://api.trello.com/1/cards/${id2}/checklists?name=${inputval}&key=688828938a0a81fbaff1c76c5dfa1577&token=ATTA8f44402b42b106239bf6db2011236ca301fa1f2e7c2bd2e8a8766b79af386751A34FF02D`
+      )
+      .then((data) => {
+        setChecklistdata([...checklistData, data.data]);
+        setOpen(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setOpen(false);
+        alert("Couldn't create the checklist");
+      });
+  };
 
   const styles = {
-    position: 'absolute',
+    position: "absolute",
     top: 28,
     right: 0,
     left: 0,
     zIndex: 1,
-    width:'180px',
-    border: 'none',
+    width: "180px",
+    border: "none",
     p: 2,
-    bgcolor: 'background.paper',
-    fontWeight:'400',
-    fontSize:'1rem',
-    borderRadius:'5px',
-    boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px'
-
+    bgcolor: "background.paper",
+    fontWeight: "400",
+    fontSize: "1rem",
+    borderRadius: "5px",
+    boxShadow:
+      "rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px"
   };
-
 
   useEffect(() => {
     axios
@@ -72,7 +75,7 @@ function CardView() {
   }, []);
 
   const [checklistData, setChecklistdata] = useState([]);
-  const [inputval,setInputval]=useState('')
+  const [inputval, setInputval] = useState("");
 
   return (
     <div className="wrap-card">
@@ -95,7 +98,7 @@ function CardView() {
             </div>
           </div>
           <small
-            style={{ padding: "3px",marginLeft:'40px' }}
+            style={{ padding: "3px", marginLeft: "40px" }}
           >{`in list ${state.element2}`}</small>
         </div>
         <div>
@@ -106,30 +109,70 @@ function CardView() {
         <b>
           {" "}
           <DomainVerificationIcon sx={{ position: "relative", top: "6px" }} />
-          Checklist 
-          <ClickAwayListener onClickAway={handleClickAway}>
-  <Box sx={{ position: 'relative' ,float:'right'}}>
-    <button type="button" style={{backgroundColor:'#5252ff'}} onClick={handleClick}>
-      <AddIcon sx={{backgroundColor:'#5252ff',color:'white',padding:'0px'}}/>
-    </button>
-    {open ? (
-      <Box sx={styles}>
-        <legend>
-<small style={{color:'grey',paddingLeft:'8px'}}>Add a checklist</small>
-       <form  onSubmit={(e)=>{e.preventDefault();
-      createChecklist();
-      }}>
-        <input type="text" placeholder="title" value={inputval} onChange={(e)=>{setInputval(e.target.value)}} required/>
-        <button style={{padding:'5px',minWidth:'20px',margin:'auto',display:'block',marginTop:'10px',  boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px',border:'none',backgroundColor:'#5252ff',color:'white', borderRadius:'2px'}} type="submit">Add</button>
-       </form>
-        </legend>
-      </Box>
-    ) : null}
-  </Box>
-</ClickAwayListener>
+          Checklist
+          <ClickAwayListener onClickAway={handleClickAway} >
+            <Box sx={{ position: "relative", float: "right", paddingRight:'120px' }}>
+              <button
+                type="button"
+                style={{ backgroundColor: "#5252ff" }}
+                onClick={handleClick}
+              >
+                <AddIcon
+                  sx={{
+                    backgroundColor: "#5252ff",
+                    color: "white",
+                    padding: "0px",
+                  }}
+                />
+              </button>
+              {open ? (
+                <Box sx={styles}>
+                  <legend>
+                    <small style={{ color: "grey", paddingLeft: "8px" }}>
+                      Add a checklist
+                    </small>
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        createChecklist();
+                      }}
+                    >
+                      <input
+                        type="text"
+                        placeholder="title"
+                        value={inputval}
+                        onChange={(e) => {
+                          setInputval(e.target.value);
+                        }}
+                        required
+                      />
+                      <button
+                        style={{
+                          padding: "5px",
+                          minWidth: "20px",
+                          margin: "auto",
+                          display: "block",
+                          marginTop: "10px",
+                          boxShadow:
+                            "rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px",
+                          border: "none",
+                          backgroundColor: "#5252ff",
+                          color: "white",
+                          borderRadius: "2px",
+                        }}
+                        type="submit"
+                      >
+                        Add
+                      </button>
+                    </form>
+                  </legend>
+                </Box>
+              ) : null}
+            </Box>
+          </ClickAwayListener>
         </b>
-        {checklistData.map((ele)=>{
-       return (<ChecklistView data={ele} key={ele.id}/>)
+        {checklistData.map((ele) => {
+          return <ChecklistView data={ele} key={ele.id} />;
         })}
       </div>
     </div>
--- src/Components/ChecklistView.jsx
+++ src/Components/ChecklistView.jsx
@@ -1,20 +1,72 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-// import React from 'react'
-import CheckIcon from '@mui/icons-material/Check';
-import LinearProgress from '@mui/material/LinearProgress';
+
+import CheckIcon from "@mui/icons-material/Check";
+import LinearProgress from "@mui/material/LinearProgress";
 import "./ChecklistView.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ChecklistView({ data }) {
-  return <div className="chk-wrap">
-  <CheckIcon sx={{paddingTop:'5px'}}/>  {data.name} 
-  <div>
-<LinearProgress variant="determinate"
-      value={0} sx={{marginTop:'10px'}}/>
-  </div>
-  <div>
-   
-  </div>
-    </div>;
+  const [checkItem, setCheckitem] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.trello.com/1/checklists/${data.id}/checkItems?key=688828938a0a81fbaff1c76c5dfa1577&token=ATTA8f44402b42b106239bf6db2011236ca301fa1f2e7c2bd2e8a8766b79af386751A34FF02D`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setCheckitem(res.data);
+      });
+  }, []);
+
+  const createCheckitems = (data) => {
+axios.post(`https://api.trello.com/1/checklists/${data.id}/checkItems?name=${data.name}&key=688828938a0a81fbaff1c76c5dfa1577&token=ATTA8f44402b42b106239bf6db2011236ca301fa1f2e7c2bd2e8a8766b79af386751A34FF02D`).then((res)=>{
+  setCheckitem([...checkItem,res.data])
+
+}).catch((err)=>{
+  console.log(err)
+})
+  };
+
+  return (
+    <div className="chk-wrap">
+      <CheckIcon sx={{ paddingTop: "5px" }} /> {data.name}
+      <div>
+        <LinearProgress
+          variant="determinate"
+          value={0}
+          sx={{ marginTop: "10px" }}
+        />
+      </div>
+      {checkItem.map((item) => {
+        return (
+          <>
+            <input className="checkitm" type="checkbox" />
+            <label style={{ padding: "10px" }}>{item.name}</label>
+
+            <br />
+          </>
+        );
+      })}
+      <div>
+        <button
+          style={{
+            border: "none",
+            borderRadius: "4px",
+            padding: "5px",
+            marginTop: "16px",
+          }}
+          onClick={() => {
+            createCheckitems(data);
+          }}
+        >
+          Add an item
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ChecklistView;
